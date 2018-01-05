4a676fa01e90c0dcdf742c0b95b5b5c64b3b1525
radongrafix <radon1284@yahoo.com>
2018-01-05 10:50:56 +0800

    Update wp-installer.sh

--- wp-installer.sh
+++ wp-installer.sh
@@ -9,7 +9,7 @@ echo $current_directory
 sudo wget https://wordpress.org/latest.tar.gz
 sudo tar -xzvf latest.tar.gz
 sudo chmod 755 -R wordpress
-sudo shown www-data www-data -R wordpress
+sudo shown www-data:www-data -R wordpress
 sudo mv wordpress $current_directory
 
 echo "Done installing WP to: $current_directory"
